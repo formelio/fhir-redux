04e4579c553202fc8ac2c677e39f5a44dc099a95
Jake Jongejans <40899982+Javernus@users.noreply.github.com>
2026-07-06 08:14:35 +0200

    feat: Client struct for get and paging operations

--- src/r5/resources.rs
+++ src/r5/resources.rs
@@ -1,3 +1,4 @@
+use crate::codes::LinkRelationTypes;
 use crate::r5::types;
 use crate::resources::{
     ActivityDefinition, CarePlan, CareTeam, Organization, Patient, PlanDefinition, Practitioner, PractitionerRole,
@@ -11,6 +12,12 @@ type_struct!(Bundle {
     pub link: Vec<types::BundleLink>,
     pub entry: Vec<types::BundleEntry>,
 });
+
+impl Bundle {
+    pub fn next(&self) -> Option<String> {
+        self.link.iter().find(|link| link.relation == LinkRelationTypes::Next).map(|link| link.url.clone())
+    }
+}
 
 resource!([
     ActivityDefinition,
